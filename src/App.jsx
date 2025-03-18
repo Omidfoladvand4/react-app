7ec95d54bc93d8a6adb3456f2c00449cd4d87b23
omid <Omidfoladvand0919@gmail.com>
2025-03-18 18:46:44 +0330

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import About from "../pages/aboutme/aboutme";
-import Projects from "../pages/projects/projects";
+import About from "./pages/aboutme/AboutMe";
+import Projects from "./pages/projects/Projects";
 import Header from "./components/header/Header";
 
 function App() {
   return (
-    <>
     <Router>
       <Navbar />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Header />
     </Router>
-      <Header></Header>
-      </>
   );
 }
 
